a19d887d96f398752f413b33ff44cf57dc496210
Michal Szpakowski <szpakk@gmail.com>
2017-08-28 17:48:47 +0200

    Add CRUD actions

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -7,6 +7,34 @@ class RostersController < ApplicationController
     @roster = Roster.new
     params[:players].each { |player| @roster.players << Player.find(player) }
     @roster.save
+    redirect_to roster_path(@roster)
   end
 
+  def show
+    @roster = Roster.find(params[:id])
+  end
+
+  def edit
+    @roster = Roster.find(params[:id])
+    @players = Player.all
+  end
+
+  def update
+    @roster = Roster.find(params[:id])
+    @roster.selections.delete_all
+    params[:players].each { |player| @roster.players << Player.find(player) }
+    @roster.save
+    redirect_to roster_path(@roster)
+  end
+
+  def index
+    @rosters = Roster.all
+  end
+
+  def destroy
+    @roster = Roster.find(params[:id])
+    @roster.selections.delete_all
+    @roster.delete
+    redirect_to new_roster_path
+  end
 end
