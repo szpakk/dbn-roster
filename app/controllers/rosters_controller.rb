62d8054e0deae058c7a143fead991804a315b287
Michal Szpakowski <szpakk@gmail.com>
2017-08-28 15:34:08 +0200

    Add create method to roster controller

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -2,4 +2,11 @@ class RostersController < ApplicationController
   def new
     @players = Player.all
   end
+
+  def create
+    @roster = Roster.new
+    params[:players].each { |player| @roster.players << Player.find(player) }
+    @roster.save
+  end
+
 end
