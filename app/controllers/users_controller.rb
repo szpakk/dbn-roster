6665a9428d25f90da89e07742c27f4eddbd16e7e
Michal Szpakowski <szpakk@gmail.com>
2017-08-30 01:55:00 +0200

    Add check login before action to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:new, :create]
+
   def index
     @users = User.all
   end
@@ -18,12 +20,25 @@ class UsersController < ApplicationController
   end
 
   def show
-    user = User.find(params[:id])
-    roster = Roster.where(:user_id => user.id).first
+    user = User.find_by(id: params[:id])
+    roster = Roster.find_by(:user_id => user.id) if user
     if roster.nil?
       redirect_to new_roster_path
     else 
       redirect_to roster_path(roster)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
   end
 end
