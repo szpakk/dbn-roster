b0696285dbfcdc8788f2ffdf55d14acd238a890c
Michal Szpakowski <szpakk@gmail.com>
2017-08-29 19:55:54 +0200

    Add user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+  end
+
+  def create
+    User.create(:name => params[:name], :password => params[:password], :password_confirmation => params[:password])
+    redirect_to new_roster_path
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
+  def show
+    user = User.find(params[:id])
+    roster = Roster.where(:user_id => user.id).first
+    if roster.nil?
+      redirect_to new_roster_path
+    else 
+      redirect_to roster_path(roster)
+    end
+  end
+end
