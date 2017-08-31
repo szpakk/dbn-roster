8e2fd0b6ab546209f58b72a0e249233a4bbcd7a9
Michal Szpakowski <szpakk@gmail.com>
2017-08-31 08:49:53 +0200

    Add verifications to player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,9 @@
 class Player < ApplicationRecord
+  validates :name, presence: true, length: { in: 2..50 },
+                                   uniqueness: { case_sensitive: false }
+  validates :position, presence: true, length: { minimum: 1 },
+                                   inclusion: { in: %w[QB RB FB WR TE OL DL LB CB S K P LS] }
+
   def self.positions
     %w[QB RB FB WR TE OL DL LB CB S K P LS]
   end
