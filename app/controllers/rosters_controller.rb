0297127a247f022992cb672dd62567288caf6fd8
Michal Szpakowski <szpakk@gmail.com>
2017-08-31 09:29:04 +0200

    Allow edit roster action to roster user

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -1,6 +1,8 @@
 class RostersController < ApplicationController
+  before_action :proper_user, only: [:edit, :update, :destroy]
   def new
     if !logged_in?
+      flash[:danger] = "Please log in"
       redirect_to login_path
     elsif !current_user.roster.nil?
       redirect_to current_user.roster
@@ -19,11 +21,6 @@ class RostersController < ApplicationController
 
   def show
     @roster = Roster.find_by(id: params[:id])
-    if current_user.roster.nil?
-      redirect_to new_roster_path
-    elsif @roster != current_user.roster
-      redirect_to current_user.roster
-    end
   end
 
   def edit
@@ -40,7 +37,8 @@ class RostersController < ApplicationController
   end
 
   def index
-    @rosters = Roster.all
+    admin = User.find_by(:admin => true)
+    @rosters = Roster.where.not(:user_id => admin.id)
   end
 
   def destroy
@@ -48,5 +46,15 @@ class RostersController < ApplicationController
     @roster.selections.delete_all
     @roster.delete
     redirect_to new_roster_path
+  end
+
+  private
+
+  def proper_user
+    roster = Roster.find_by(id: params[:id])
+    unless logged_in? && current_user == roster.user
+      flash[:danger] = "Access denied!"
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
