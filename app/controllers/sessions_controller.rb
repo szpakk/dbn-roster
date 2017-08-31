349d019fa469824526e95024eb27747538a7e32f
Michal Szpakowski <szpakk@gmail.com>
2017-08-31 09:30:42 +0200

    Minor change flash message for invalid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to user
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash[:danger] = 'Invalid username/password combination'
       render 'new'
     end
   end
