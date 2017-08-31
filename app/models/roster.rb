9bb41ead377a02ba14f729a31c4290d0d5ac1a23
Michal Szpakowski <szpakk@gmail.com>
2017-08-31 19:03:58 +0200

    Add roster correctness stat

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -10,6 +10,13 @@ class Roster < ApplicationRecord
     Roster.find_by(final: true)
   end
 
+  def result
+    return 0.0 if Roster.final_roster.nil?
+    count = 0.0
+    self.players.each { |player| count += 1 if Roster.final_roster.players.include?(player) }
+    count / 0.53
+  end
+
   def is_there_final?
     errors.add(:rosters, 'Final roster exists') unless final == false || Roster.final_roster.nil?
   end
